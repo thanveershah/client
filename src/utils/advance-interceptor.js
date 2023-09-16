9479cedb7379eac59ded4e8efd04710f4a281636
thanveershah <thanveershah92@gmail.com>
2023-09-16 20:30:04 +0400

    updated urkl

--- src/utils/advance-interceptor.js
+++ src/utils/advance-interceptor.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-   baseURL: "http://localhost:3003/api",
+   baseURL: "https://server-beta-taupe.vercel.app/api",
 });
 
 // 401 - UnAuthorised
